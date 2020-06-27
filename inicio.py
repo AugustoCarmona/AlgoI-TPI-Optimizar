7085947139885022c6adbf50c9089c95ce06f907
josepineiro1 <65984110+josepineiro1@users.noreply.github.com>
2020-06-27 19:09:24 -0300

    version 2.0

---
+++ inicio.py
@@ -0,0 +1,18 @@
+def abro (programa):
+    listaP= programa.readline()
+    lineaP= open(listaP,"r")
+    return (listaP)
+
+def grabado(archivo,linea):
+    archivo.write(linea + "/n")
+    
+def separacion (archivo):
+    linea= archivo.readline()
+    while linea
+        if "#" in linea:
+            grabado(comentarios.cvs,linea)
+        elif linea not "":
+            #me fijo si no es vacio
+            grabado(fuente_unico.csv,linea)
+        
+        linea= archivo.readline()
