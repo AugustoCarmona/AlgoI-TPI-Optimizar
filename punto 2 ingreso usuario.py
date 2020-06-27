34b7da16a3227856c34cc8200417407b3264fc0a
josepineiro1 <65984110+josepineiro1@users.noreply.github.com>
2020-06-27 19:55:59 -0300

    version 3

--- punto 2 ingreso usuario.py
+++ punto 2 ingreso usuario.py
@@ -12,9 +12,11 @@ def ingresoUsuario(lista):
                 #igual que arriba pero con
                 #lo relativo
         elif ingreso =="?todo" or ingreso == "#todo":
-            # abrimos el archivo y lo imprimimos entero
+            with open (descripciones.txt) as descripciones:
+                descripciones.readline()
         elif ingreso == "inprimir?todo":
-            #no entendi lo que pedia aca
+            with open(ayuda_funciones.txt,"r") as archivo:
+                archivo.readline()
         else:
             print("ingreso invalido, porfavor ingrese un nombre de funcion valido o un comando valido")
         igreso=str(imput("Funcion:"))
