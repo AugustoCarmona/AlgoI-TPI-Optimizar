8ba397e3bf64427b8c7e38c414db6ef5c44c8eac
josepineiro1 <65984110+josepineiro1@users.noreply.github.com>
2020-06-30 19:17:25 -0300

    cambios 30 de junio a las 19:17
    ideas generales de cada punto

---
+++ punto 5 idea.py
@@ -0,0 +1,9 @@
+"""idea 5
+al igual que en los anteriores, en este igual tenemos que poner al inicio "informe de Desarrollo
+Por Autor" y poner el autor con eso me parece que establecer dos funciones, una que tenga
+para poner los nombres y otra para poner lo de adentro. Vamos contando las lineas que escribio
+les ponemos un contador que cuente las lineas y para el resumen no se si va la suma de todas las
+lineas de las funciones o de las cuales el autor escribio, tampoco se si es solo codigo o
+los comentarios tambien. Tambien en el total final tenemos que tener o otra funcion
+o que la misma que pone los autores una vez termine con el archivo que tiene la lisa
+de los autores imprima el final"""
