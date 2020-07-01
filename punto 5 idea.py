107d3fc6a080f12806da0d957c333a1bd41ab0e9
josepineiro1 <65984110+josepineiro1@users.noreply.github.com>
2020-06-30 22:34:20 -0300

    intente hacer algo con el 5
    el archivo se llama tony porque paso por muchos cambios ese archivo de texto y no pense hacerlo el original (joke on my)

--- punto 5 idea.py
+++ punto 5 idea.py
@@ -1,9 +1,41 @@
-"""idea 5
-al igual que en los anteriores, en este igual tenemos que poner al inicio "informe de Desarrollo
-Por Autor" y poner el autor con eso me parece que establecer dos funciones, una que tenga
-para poner los nombres y otra para poner lo de adentro. Vamos contando las lineas que escribio
-les ponemos un contador que cuente las lineas y para el resumen no se si va la suma de todas las
-lineas de las funciones o de las cuales el autor escribio, tampoco se si es solo codigo o
-los comentarios tambien. Tambien en el total final tenemos que tener o otra funcion
-o que la misma que pone los autores una vez termine con el archivo que tiene la lisa
-de los autores imprima el final"""
+#falta forma de definir cuando es un nuevo autor y depende mucho del archivo en y el orden pero
+#imagine que el archivo lo haciamos nosotros asi que no habria drama
+tony=open("ejemplo.txt","r")
+numeros="12345678990"
+funciones=0
+lineas=0
+cantidad_de_lineas=43
+autor=tony.readline()
+funcion = tony.readline().split(',')
+linea = tony.readline().split()
+total_f=0
+total_l=0
+line="0"
+porcentaje=(lineas//cantidad_de_lineas)*100
+funcion_nombres=[]
+print ("%-1s %s" %("Autor:", autor))
+print("%-6s %-20s %s" %("","funcion", "lineas"))
+print(("----------------------------------"))
+while linea!=[''] and autor!=[]:
+    if line in numeros:
+        print("%-6s %-20s %s" %("",funcion,linea))
+        line=linea[0]
+        funciones+=1
+        if funcion not in funcion_nombres:
+            total_f+=1
+            funcion_nombres+=funcion
+        lineas+=int(line)
+        funcion = tony.readline().split()
+        linea = tony.readline().split()
+        porcentaje=((lineas/cantidad_de_lineas))*100
+        total_l+=1
+    else:
+        print("%-6s %s %-19s %s %s %s" %("",funciones,"funciones - lineas",lineas,porcentaje,"%"))
+        autor=funcion
+        funciones=0
+        lineas=0
+        print ("%-1s %s" %("Autor:", autor))
+        funcion = tony.readline().split()
+        linea = tony.readline().split()
+print("%s %-6s %-20s %s"%("total:", total_f, "funciones - lineas",total_l))
+ejemplo.close()
