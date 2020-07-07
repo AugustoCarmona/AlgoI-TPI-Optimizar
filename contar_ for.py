ec3d69bad8599c70560eb3de344843eec2b9a51a
guzman84 <67488616+guzman84@users.noreply.github.com>
2020-07-07 18:18:03 -0300

    intento del contador de for

---
+++ contar_ for.py
@@ -0,0 +1,27 @@
+#intento de buscador de ciclos for y while
+
+def buscador (archivo):
+    
+    with open (archivo.csv,"r") as linea
+    
+    lineaF = linea.readline()
+    
+    F_contador = 0
+    
+    while :#con alguna condicon que  no se me ocurre aun
+        
+        if lineaf != "" and "for " in lineaF :
+            
+            F_contador +=1
+            
+        lineaF = linea.readline()
+        
+        
+    return contador
+
+              
+    
+
+    
+    
+    
