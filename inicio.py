f12db4fc1e22f863e3080df40b99f9b15fbef2ec
gastonmondin <65984239+gastonmondin@users.noreply.github.com>
2020-07-10 19:43:50 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,18 +1,38 @@
-def abro (programa):
-    listaP= programa.readline()
-    lineaP= open(listaP,"r")
-    return (listaP)
+def crear_lista():
+    with open("programas.txt","r") as archivo_programas:
+        programas=[]
+        linea=archivo_programas.readline().rstrip("\n")
+        while linea!="":
+            programas.append(linea)
+            linea=archivo_programas.readline().rstrip("\n")
+    return programas
 
-def grabado(archivo,linea):
-    archivo.write(linea + "/n")
-    
-def separacion (archivo):
-    linea= archivo.readline()
-    while linea
-        if "#" in linea:
-            grabado(comentarios.cvs,linea)
-        elif linea not "":
-            #me fijo si no es vacio
-            grabado(fuente_unico.csv,linea)
-        
-        linea= archivo.readline()
+def crear_tabla(programas):
+    tabla={}
+    for programa in programas:
+        with open(programa,"r") as archivo_programa:
+            funcion_actual=""
+            comentado=False
+            for linea in archivo_programa:
+                linea=linea.strip("    ").strip("\n")
+                if "#" in linea:
+                    linea=linea[:linea.index("#")]
+                if "def " in linea:
+                    funcion_actual=linea[linea.index(" ")+1:linea.index("(")]
+                    tabla[funcion_actual]=[]
+                    tabla[funcion_actual].append(linea[linea.index("("):linea.index(":")])
+                    tabla[funcion_actual].append(programa)
+                elif funcion_actual!="" and linea!="":
+                    if '"""' in linea and comentado:
+                        comentado=False
+                    elif '"""' in linea and not comentado:
+                        comentado=True
+                    elif not comentado:
+                        tabla[funcion_actual].append(linea)
+    return tabla
+
+
+programas=crear_lista()
+tabla=crear_tabla(programas)
+for funcion in sorted(tabla):
+    print(funcion)
