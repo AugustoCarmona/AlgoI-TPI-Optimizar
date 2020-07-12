149425e16bb6d6eda86e8f70d210bcd6ab48005d
josepineiro1 <65984110+josepineiro1@users.noreply.github.com>
2020-07-12 15:58:08 -0300

    merge real

--- merge.py
+++ merge.py
@@ -8,18 +8,26 @@ def merge(diccionario,archivo):
             lista_Completa_Pero_No_Ordenada.append(diccionario[clave][item])
             item+=1
     lista_Completa_Y_Ordenada=sorted(lista_Completa_Pero_No_Ordenada)
-    for programa in lista_Completa_Y_Ordenada:
-        lugar = 0
-        while not programa in diccionario[lista_de_archivos[lugar]]:
-            lugar+=1
-        guardar(programa,archivo)
+    abrir(lista_Completa_Y_Ordenada,diccionario,lista_de_archivos)
     
+def abrir(lista_Completa_Y_Ordenada,diccionario,lista_de_archivos):
+    lista_de_programas={}
+    for archivo in lista_de_archivos:
+        index=0
+        valor=open(archivo,"r")
+        while index < len(diccionario[archivo]):
+            linea=valor.readline().rstrip('\n')
+            lista_de_programas[diccionario[archivo][index]]=linea
+            index+=1
+        valor.close()
+    for funcion in lista_Completa_Y_Ordenada:
+        linea_a_guardar=lista_de_programas[funcion]
+        guardar(linea_a_guardar,union)
 def guardar(nombre,archivo):
     
     archivo.write(nombre+"\n")
 
-
-diccionario={"app_matematica_codigo":["menu_MCD","menu_MCM","menu_elegir","menu_factorial","menu_opciones","menu_potencia","menu_primo","solicitar_valor"],"lib_matematica_codigo":["es_primo","factorial","mcd","mcm","potencia"]}
+diccionario={"app_matematica_codigo.csv":["menu_MCD","menu_MCM","menu_elegir","menu_factorial","menu_opciones","menu_potencia","menu_primo","solicitar_valor"],"lib_matematica_codigo.csv":["es_primo","factorial","mcd","mcm","potencia"]}
 union=open("union.csv","w")
 merge(diccionario,union)
 union.close()
