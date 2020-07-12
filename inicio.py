af15bd536b22858f58c7eb0d39080bdaa53ec96b
gastonmondin <65984239+gastonmondin@users.noreply.github.com>
2020-07-12 19:26:53 -0300

    Nueva versión op

--- inicio.py
+++ inicio.py
@@ -7,44 +7,67 @@ def crear_lista():
             linea=archivo_programas.readline().rstrip("\n")
     return programas
 
-#def crear_comentarios_archivos()
+def generar_archivos_csv(programa, codigo, comentarios):
+    with open(programa.strip(".py")+"_codigo.csv","w+") as archivo_codigo:
+        for items in sorted(codigo):
+            archivo_codigo.writelines(items)
+            for item in codigo[items]:
+                archivo_codigo.writelines(";"+item)
+            archivo_codigo.writelines("\n")
+    with open(programa.strip(".py")+"_comentarios.csv","w+") as archivo_comentarios:
+        for items in sorted(comentarios):
+            archivo_comentarios.writelines(items)
+            for item in comentarios[items]:
+                archivo_comentarios.writelines(";"+item)
+            archivo_comentarios.writelines("\n")
 
-def crear_archivos(programas):
+
+def analizar_lineas(archivo, programa, codigo, comentarios):
+    funcion_actual=""
+    string=""
+    comentado=False
+    for linea in archivo:
+        linea=linea.strip("    ").strip("\n")
+        if "#" in linea and funcion_actual=="":
+                linea=linea[:linea.index("#")]
+        elif "#" in linea:
+            comentarios[funcion_actual].append(linea[linea.index("#")+2:])
+            linea=linea[:linea.index("#")]
+        if "def " in linea:
+            funcion_actual=linea[linea.index(" ")+1:linea.index("(")]
+            codigo[funcion_actual]=[]
+            comentarios[funcion_actual]=[]
+            codigo[funcion_actual].append(linea[linea.index("("):linea.index(":")])
+            codigo[funcion_actual].append(programa)
+        elif funcion_actual!="" and linea!="":
+            if '"""' in linea and comentado:
+                comentado=False
+            elif '"""' in linea and not comentado:
+                comentado=True
+                if "[Autor:" in linea:
+                    comentarios[funcion_actual].append(linea[linea.index("["):])
+            elif not comentado:
+                codigo[funcion_actual].append(linea)
+            elif comentado:
+                if "[Ayuda: " in linea and "]" in linea:
+                    comentarios[funcion_actual].append(linea)
+                elif "[Ayuda: " in linea:
+                    string=""
+                    string+=linea+" "
+                elif "]" in linea:
+                    string+=linea
+                    comentarios[funcion_actual].append(string)
+                else:
+                    string+=linea+" "
+    generar_archivos_csv(programa, codigo, comentarios)
+
+def recorrer_programas(programas):
     for programa in programas:
         codigo={}
         comentarios={}
         with open(programa,"r") as archivo:
-            funcion_actual=""
-            comentado=False
-            for linea in archivo:
-                linea=linea.strip("    ").strip("\n")
-                if "#" in linea and funcion_actual=="":
-                    linea=linea[:linea.index("#")]
-                elif "#" in linea:
-                    comentarios[funcion_actual].append(linea[linea.index("#"):])
-                    linea=linea[:linea.index("#")]
-                if "def " in linea:
-                    funcion_actual=linea[linea.index(" ")+1:linea.index("(")]
-                    codigo[funcion_actual]=[]
-                    comentarios[funcion_actual]=[]
-                    codigo[funcion_actual].append(linea[linea.index("("):linea.index(":")])
-                    codigo[funcion_actual].append(programa)
-                
-                elif funcion_actual!="" and linea!="":
-                    if '"""' in linea and comentado:
-                        comentado=False
-                    elif '"""' in linea and not comentado:
-                        comentado=True
-                    elif not comentado:
-                        codigo[funcion_actual].append(linea)
-            with open(programa.strip(".py")+"_codigo.csv","w+") as archivo_codigo:
-                for items in sorted(codigo):
-                    archivo_codigo.writelines(items)
-                    for item in codigo[items]:
-                        archivo_codigo.writelines(","+item)
-                    archivo_codigo.writelines("\n")       
-    print(comentarios)         
+            analizar_lineas(archivo, programa, codigo, comentarios)       
 
 programas=crear_lista()
 #crear_tabla(programas)
-crear_archivos(programas)
+recorrer_programas(programas)
