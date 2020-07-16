07c8a038774f82fe359a3710dc5e56eb5cc5f44b
lucianosolis <51059867+lucianosolis@users.noreply.github.com>
2020-07-15 23:23:11 -0300

    version 5
    falta hacer la tabla y agregar 2 condiciones

--- punto1.py
+++ punto1.py
@@ -3,7 +3,10 @@
 
 
 def contador(archivo,lista_codigo):
-          
+          """[Autor:Luciano Solis]
+             [Ayuda: Esta funcion recibe una lista anidada y un archivo vacio.Lo que hace esta funcion es juntar informacion
+             de la cantidad de veces que se utiliza cada funcion, como asi tambien los comentarios, y esa informacion la guarda en un archivo
+          """   
           for posicion in range(len(lista_codigo)):
               diccionario={}
               nombre_funcion=""
@@ -57,10 +60,27 @@ def contador(archivo,lista_codigo):
               merge_comentarios.seek(0)
               diccionario={"nombre funcion":nombre_funcion,"nombre_modulo":nombre_modulo,"parametros":cont_parametr,"returns":cont_return,"elif/if":cont_if,"for":cont_for,"while":cont_while,"break":cont_break,"exit":cont_exit,"comentarios":cont_coment,"autor":autor,"ayuda":ayuda,"invocaciones":invocaciones}
               print(diccionario)
+              
               archivo.write(str(nombre_funcion) +";" + str(nombre_modulo) + ";" + str(cont_parametr) +";"+ str(cont_return)+";"+str(cont_if)+";"+str(cont_for)+";"+str(cont_while)+";"+str(cont_break) +";"+str(cont_exit)+";"+str(cont_coment)+"\n")
             
 
 def armado_listas_fuente_unico(merge):
+    """[Autor:Luciano Solis]
+       [Ayuda: Lo que hace esta funcion es recibir un archivo que contiene codigo y devuelve una lista anidada con toda la informacion del archivo]
+    """
+    linea=merge_fuente_unico.readline().strip("\n").split(";")
+    lista_final=[]
+    while linea!=[""]:
+         lista_final.append(linea)
+         linea=merge_fuente_unico.readline().split(";")
+ 
+    return lista_final
+  
+
+def armado_listas_comentarios(merge):
+    """[Autor:Luciano Solis]
+       [Ayuda: Lo que hace esta funcion es recibir un archivo que contiene comentarios y devuelve una lista anidada con toda la informacion del archivo]
+    """
     linea=merge_fuente_unico.readline().rstrip("\n").split(";")
     lista_final=[]
     while linea!=[""]:
@@ -68,19 +88,13 @@ def armado_listas_fuente_unico(merge):
          linea=merge_fuente_unico.readline().split(";")
  
     return lista_final
-
-
-def armado_listas_comentarios(merge):
-   linea=merge_fuente_unico.readline().rstrip("\n").split(";")
-   lista_final=[]
-   while linea!=[""]:
-         lista_final.append(linea)
-         linea=merge_fuente_unico.readline().split(";")
- 
-   return lista_final
   
 
 def cantidad_de_invocaciones(lista_codigo,nombre_funcion,posicion):
+     """[Autor:Luciano Solis]
+        [Ayuda: Esta funcion recibe una lista anidada, un elemento de una sublista que es una funcion y una posicion de la lista anidada
+       y lo que devuelve es la cantidad de veces que se invoco al elemento funcion en la lista anidada
+     """
      cont_invocaciones=0
      for posicion_2 in range(len(lista_codigo)):
           for caracter_2 in range(len(lista_codigo[posicion_2])):
@@ -90,9 +104,6 @@ def cantidad_de_invocaciones(lista_codigo,nombre_funcion,posicion):
     
      return cont_invocaciones
 
-
-
-
 #---------------------------bloque principal -----------------------------------------
 archivo=open("panel_general.csv","w")
 merge_fuente_unico=open("app_matematica_codigo.csv","r")
@@ -101,4 +112,4 @@ lista_merge_fuente_unico=armado_listas_fuente_unico(merge_fuente_unico)
 #lista_comentarios=armado_listas_comentarios(merge_comentarios)
 contador(archivo,lista_merge_fuente_unico)
 archivo.close()
-
+print(lista_merge_fuente_unico)
