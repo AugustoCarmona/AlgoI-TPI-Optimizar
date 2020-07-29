5477544e9d8373c57163cb3cb9243f4a1b7a75e3
josepineiro1 <65984110+josepineiro1@users.noreply.github.com>
2020-07-29 20:45:25 -0300

    UP-DATE

--- diagrama_arbol.py
+++ diagrama_arbol.py
@@ -1,139 +1,134 @@
-def saco_info ():
-    """ [Autor:Jose Piñeiro]
-        [Ayuda:Esta funcion abre el archivo fuente_unico y extrae el codigo de cada una y su nombre]
+def obtener_informacion(): #invocada desde main()
+    """ [Autor: Augusto Carmona, Jose Piñeiro]
+        [Ayuda: abre el archivo fuente_unico y extrae el codigo de cada funcion y su nombre el cual
+        evaluara con la funcion procesar_diccionario() que nos dara  los datos necesarios para armar
+        el diagrama de arbol]
     """
     codigos = open("data\\fuente_unico.csv","r")
     linea = codigos.readline().split(";")
     funciones = {}
     
-    while(linea != [""]):
+    while(linea != [""]): #extrae el crudo del archivo fuente_unico.csv
         funciones[linea[0]]=linea[3:]
         linea = codigos.readline().split(";")
     codigos.close()
 
-    return(funciones)
-
-#------------------------------------------------------------
-def busco_funciones(lista):
-    """ [Autor:Jose Piñeiro]
-        [Ayuda:Esta funcion dice quien llama a quien y cuantas lineas de codigo hay de cada funcion]
-    """
-    funciones_nombres = {}
-    diccionario_Arbol = {}
-
-    for funcion in lista:
-        funciones_nombres[funcion] = [len(lista[funcion])]#cantidad de codigo de la funcion
+    diccionario_final = procesar_diccionario(funciones) #procesa fuente_unico.csv y le da el formato necesitado
     
-    for funcion in funciones_nombres:
-        for linea in lista:
-            index = 0
-            while index < len(lista[linea]):
-                parte_por_parte=lista[linea][index].split(" ")
-                parte_index=0
-                while parte_index < len(parte_por_parte):
-                    if (funcion == parte_por_parte[parte_index]) or ((funcion in parte_por_parte[parte_index]) and ("." in parte_por_parte[parte_index])):
-                        if not linea in diccionario_Arbol:
-                            diccionario_Arbol[linea] = [[funcion]]
-                        else:
-                            diccionario_Arbol[linea][0].append(funcion)
-                    elif ("(" in parte_por_parte[parte_index]):
-                        lugar_parentesis = parte_por_parte[parte_index].find("(")
-                        lugar_primera_letra_funcion = parte_por_parte[parte_index].find(funcion)
-                        if (lugar_primera_letra_funcion > 0) and (not (parte_por_parte[parte_index][lugar_primera_letra_funcion - 1].isalpha) or (parte_por_parte[parte_index][lugar_primera_letra_funcion - 1] != "_")):
-                            if (parte_por_parte[parte_index][lugar_parentesis] == "(" ):
-                                if not linea in diccionario_Arbol:
-                                    diccionario_Arbol[linea] = [[funcion]]
-                                else:
-                                    diccionario_Arbol[linea][0].append(funcion)
-                        elif (lugar_primera_letra_funcion == 0) and (lugar_parentesis != 0) and (funcion in parte_por_parte[parte_index]):
-                            if (parte_por_parte[parte_index][lugar_primera_letra_funcion + len(funcion)] == "(" ):
-                                if not linea in diccionario_Arbol:
-                                    diccionario_Arbol[linea] = [[funcion]]
-                                else:
-                                    diccionario_Arbol[linea][0].append(funcion)
-
-                    parte_index+=1
-                index += 1
-    
-    uniones = union(diccionario_Arbol,funciones_nombres)
-    return uniones
-
-#------------------------------------------------------------
-def union(llamados,cantidad_lineas):
-    """ [Autor:Jose Piñeiro]
-        [Ayuda:Esta funcion une en un solo diccionario dando la cantidad de lineas de codigo de cada funcion y 
-        que funcion es llamada]
-    """
-    no_llamados = []
-    lista_finale = llamados
-    
-    for funcion in cantidad_lineas:
-        no_llamados.append(funcion)
-    for los_que_llaman in llamados:
-        lista_finale[los_que_llaman].append(cantidad_lineas[los_que_llaman])
-        no_llamados.remove(los_que_llaman)
-    for los_llamados in no_llamados:
-        lista_finale[los_llamados] = ("", cantidad_lineas[los_llamados])
-    
-    return lista_finale
-
-#------------------------------------------------------------
-def obtener_informacion():
-    """ [Autor:Jose Piñeiro]
-        [Ayuda:Esta funcion llama al resto de las funciones, para devolver 
-        el diccionario utilizado en el punto 4]
-    """
-    diccionario = saco_info()
-    diccionario_final = busco_funciones(diccionario)
     return diccionario_final
 
-#---------------------------------#
-def buscar_principal(diccionario):
-    """ [Autor: Augusto Carmona]
-        [Ayuda: identifica a la funcion que llama a más funciones del programa]
+#------------------------------------------------------------
+def procesar_diccionario(funciones): #invocada desde obtener_informacion()
+    """ [Autor: Augusto Carmona, Jose Piñeiro]
+        [Ayuda: extrae cuantas lineas de codigo hay en cada funcion e invoca a evaluador_invocaciones() para evaluar
+        cuantas invocaciones genera]
     """
-    llamados_principal = []
-    for clave in diccionario: #por cada funcion en el diccionario
-        if len(diccionario[clave][0]) > len(llamados_principal): #si la funcion llama a mas funciones que la que se tiene como llamados_principal
-            llamados_principal = diccionario[clave][0] #pasa a ser principal
-    fue_llamada_luego = llamados_principal.copy()
-    for clave in diccionario:
-        if len(diccionario[clave][0]) >= 1:
-            for funcion in diccionario[clave][0]:
-                if not funcion in fue_llamada_luego:
-                    fue_llamada_luego.append(funcion)
-    return fue_llamada_luego,llamados_principal
+    cantidad_lineas = {}
+    llamados = {}
+    
+    for funcion in funciones:
+        cantidad_lineas[funcion] = [len(funciones[funcion])] #cantidad de lineas de codigo de la funcion
+    
+    for funcion in cantidad_lineas:
+        evaluador_invocaciones(funcion, funciones, llamados)
+        
+    
+    diccionario_unido = diccionario_unificado(llamados,cantidad_lineas)
+    
+    return diccionario_unido
 
-#---------------------------------#
+#------------------------------------------------------------
+def evaluador_invocaciones(funcion, funciones, llamados): #invocada desde procesar diccionario()
+    """ [Autor: Augusto Carmona, Jose Piñeiro]
+        [Ayuda: extrae cuantas invoaciones genra cada funcion]
+    """
+    for linea in funciones:
+        index = 0
+        
+        while index < len(funciones[linea]):
+            sub_modulos = funciones[linea][index]
+            if funcion in sub_modulos:
+                veces_que_aparece =  sub_modulos.count(funcion)
+                lugar_antiguo = 0
+                for veces in range(0,veces_que_aparece):
+                    buscador = 0
+                    lugar_primera_letra_funcion = sub_modulos[lugar_antiguo:].find(funcion) + lugar_antiguo
+                    if ((not sub_modulos[lugar_primera_letra_funcion-1].isalnum) or (not sub_modulos[lugar_primera_letra_funcion-1]== "_")) and ((not sub_modulos[lugar_primera_letra_funcion + len(funcion)].isalnum) or (not sub_modulos[lugar_primera_letra_funcion+ len(funcion)]== "_")):
+                        es_funcion = "puede ser"
+                        while es_funcion == "puede ser" or buscador == (len (sub_modulos)- (lugar_primera_letra_funcion + len(funcion))):
+                            if sub_modulos[lugar_primera_letra_funcion + len(funcion) + buscador] == "(":
+                                es_funcion = "si"
+                                if not linea in llamados:
+                                    llamados[linea] = [[funcion]]
+                                else: llamados[linea][0].append(funcion)
+
+                            elif (not sub_modulos[lugar_primera_letra_funcion + len(funcion) + buscador] == " "):
+                                es_funcion = "no"
+                            elif sub_modulos[lugar_primera_letra_funcion + len(funcion) + buscador] == " ":
+                                buscador +=1
+                    if (len(sub_modulos) > (lugar_primera_letra_funcion + len(funcion)+1)):
+                        if (not sub_modulos[lugar_primera_letra_funcion + len(funcion)] == funcion[0]):
+                            lugar_antiguo = lugar_primera_letra_funcion+1
+                    else:
+                        lugar_antiguo = lugar_primera_letra_funcion
+            index += 1
+    return llamados
+#------------------------------------------------------------
+def diccionario_unificado(llamados, cantidad_lineas): #invocada desde procesar_diccionario()
+    """ [Autor: Augusto Carmona, Jose Piñeiro]
+        [Ayuda: da estructura final al diccionario de funciones]
+    """
+    funciones_no_llamadas = []
+    lista_final = llamados
+    
+    for funcion in cantidad_lineas:
+        funciones_no_llamadas.append(funcion)
+    
+    for funciones_que_llaman in llamados:
+        lista_final[funciones_que_llaman].append(cantidad_lineas[funciones_que_llaman])
+        funciones_no_llamadas.remove(funciones_que_llaman)
+    
+    for funciones_llamadas in funciones_no_llamadas:
+        lista_final[funciones_llamadas] = ("", cantidad_lineas[funciones_llamadas])
+    
+    return lista_final
+
+#--------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------#
 def imprimir(diccionario):
-    """ [Autor: Augusto Carmona]
+    """ [Autor: Augusto Carmona, Jose Piñeiro]
         [Ayuda: imprime un diagrama de arbol indicando que funcion llama a que funcion y entre parentesis 
         la cantidad de lineas de codigo de cada una]
     """
     print()
     print("=====ARBOL DE INVOCACIONES=====================================================")
-    buscador(diccionario)
+    generar_diagrama(diccionario)
     print("===============================================================================")
     print()
 
 #---------------------------------#
-def buscador(diccionario):
-    """ [Autor: Augusto Carmona]
-        [Ayuda: buca e imprime]
+def generar_diagrama(diccionario):
+    """ [Autor: Augusto Carmona, Jose Piñeiro]
+        [Ayuda: busca en el diccionario la informacion de invocaciones y cantidad de lineas sobre cada funcion
+        y la estructura en modo de diagrama de arbol]
     """
-    llamados_secundarios , llamados_principales= buscar_principal(diccionario)
-    for clave in diccionario: #por cada clave en el diccionario
-        if (diccionario[clave][0] != "") and (not clave in llamados_secundarios): #si llama funciones y no es la principal
-            print("{}({})".format(clave, diccionario[clave][-1][0])) #se imprime
-            for funcion in diccionario[clave][0]: #por cada funcion que esta llama
+    llamados_secundarios , llamados_principales = identificar_funcion_principal(diccionario)
+
+    for clave in diccionario: 
+        if (diccionario[clave][0] != "") and (not clave in llamados_secundarios):
+            print("{}({})".format(clave, diccionario[clave][-1][0]))
+            
+            for funcion in diccionario[clave][0]:
                 print()
-                print("\t--> {}({})".format(funcion, diccionario[funcion][-1][0])) #tambien se imprimen
-                if (diccionario[funcion][0] != "") and (funcion in llamados_principales): #si llama más funciones
+                print("\t--> {}({})".format(funcion, diccionario[funcion][-1][0]))
+            
+                if (diccionario[funcion][0] != ""):
                         for elemento in diccionario[funcion][0]:
                             espacio = "\t\t"
                             ajuste= espacio + "--> {}({})"
                             print(ajuste.format(elemento, diccionario[elemento][-1][0]))
                             print()
+            
                             while diccionario[elemento][0] != "":
                                 if diccionario[elemento][0] != "":
                                     espacio += "\t"
@@ -142,17 +137,38 @@ def buscador(diccionario):
                                         print(ajuste.format(seudo_elemento, diccionario[seudo_elemento][-1][0]))
                                         print()
                                     elemento = seudo_elemento
+
+#------------------------------------------------------------
+
+def identificar_funcion_principal(diccionario): #llamada desde generar_diagrama()
+    """ [Autor: Augusto Carmona, Jose Piñeiro]
+        [Ayuda: identifica a la funcion que llama a más funciones del programa]
+    """
+    llamados_principal = []
+
+    for clave in diccionario:
+        if len(diccionario[clave][0]) > len(llamados_principal):
+            llamados_principal = diccionario[clave][0]
+    
+    fue_llamada_luego = llamados_principal.copy()
+    
+    for clave in diccionario:
+        if len(diccionario[clave][0]) >= 1:
+            for funcion in diccionario[clave][0]:
+                if not funcion in fue_llamada_luego:
+                    fue_llamada_luego.append(funcion)
+    
+    return fue_llamada_luego,llamados_principal
                         
 #------------------------------------------------------------------------------------------------------------------------#
 #--------------------------------------------------- bloque principal ---------------------------------------------------#
 #------------------------------------------------------------------------------------------------------------------------#
-def main():
-    """ [Autor: Augusto Carmona]
-        [Ayuda: abre el archivo principal, lo deriva  la funcion diccionario_general para obtener un diccionario
-        con las funciones a analizar y cierra el archivo. Una vez obtenido el diccionario general lo envia a la
-        funcion imprimir]
+def main_diagrama_arbol():
+    """ [Autor: Augusto Carmona, Jose Piñeiro]
+        [Ayuda: divide el programa en dos bloques principales, uno que extrae la
+        informacion del archivo fuente_unico.csv y otro que la imprime como un diagrama de arbol]
     """
     diccionario_funciones = obtener_informacion()
     imprimir(diccionario_funciones)
 
-main()
+main_diagrama_arbol()
